d3e5c97026fe18b4800f90b608545adfd2a66bc2
adwolfer <adwolfer@gmail.com>
2018-07-30 13:27:55 +0100

    peakPantheR_ROIStatistics save an EIC for each ROI, and calculate the mean IS RT in reference samples

---
+++ R/peakPantheR_ROIStatistics.R
@@ -0,0 +1,136 @@
+#' Save to disk each ROI EIC and mean IS RT
+#' Using reference samples (\code{referenceSpectraFiles}), save (to \code{saveFolder}) each ROI EIC (\code{ROI}) and reports the mean apex RT for all IS (\code{IS_ROI}) across samples
+#' @param referenceSpectraFiles (str) A character vector of paths to the reference spectra files
+#' @param saveFolder (str) Path to the folder where EICs and IS mean RT (\code{IS_mean_RT.csv}) will be saved
+#' @param ROI (data.frame) NULL or a data.frame of Regions Of Interest (ROI) with compounds as row and ROI parameters as columns: \code{rtMin} (float in seconds), \code{rt} (float in seconds, or \emph{NA}), \code{rtMax} (float in seconds), \code{mzMin} (float), \code{mz} (float or \emph{NA}), \code{mzMax} (float) (if NULL, ROI EICs are not saved)
+#' @param IS_ROI (data.frame) NULL or a data.frame of IS ROI with IS as row and ROI parameters as columns: \code{rtMin} (float in seconds), \code{rt} (float in seconds, or \emph{NA}), \code{rtMax} (float in seconds), \code{mzMin} (float), \code{mz} (float or \emph{NA}), \code{mzMax} (float)  (if NULL IS mean RT is not calculated and saved in \code{IS_mean_RT.csv})
+#' @param sampleColour (str) NULL or vector colour for each sample
+#' @param ncores (int) Number of cores to use to integrate IS in parallel
+#' @param saveISPlots (bool) If TRUE save a diagnostic plot for each IS to \code{saveFolder/IS_search} compound
+#' @param verbose (bool) If TRUE message progress
+#' @return None
+#' @export
+peakPantheR_ROIStatistics <- function(referenceSpectraFiles, saveFolder, ROI=NULL, IS_ROI=NULL, sampleColour=NULL, ncores=0, saveISPlots=TRUE, verbose=TRUE){
+  
+  ## Init - check input
+  # reference spectra
+  if(typeof(referenceSpectraFiles)!='character') {stop('Check input, "referenceSpectraFiles" must be a vector of spectra paths')}
+  nbSpectra <- length(referenceSpectraFiles)
+  
+  # save folder
+  if((typeof(saveFolder)!='character')|(length(saveFolder)!=1)) {stop('Check input, "saveFolder" must be a path')}
+  dir.create(saveFolder, recursive=TRUE, showWarnings=FALSE)
+  
+  # sampleColour
+  if(!is.null(sampleColour)) {
+    if((typeof(sampleColour)!='character')|(length(sampleColour)!=nbSpectra)) {
+      if(verbose) { message('Check input, "sampleColour" must be a vector of colour of same length as "referenceSpectraFile": default colour used instead') }
+      sampleColour  <- rep('black', nbSpectra)
+    }
+  } else {
+    sampleColour    <- rep('black', nbSpectra)
+  }
+  
+  # save EICs for each ROI if ROI is properly defined
+  saveEICsROI   <- FALSE
+  if(!is.null(ROI)) {
+    # ROI is a data.frame
+    if(is.data.frame(ROI)) {
+      # ROI data.frame columns
+      if(all(c("cpdID", "cpdName", "rtMin", "rt", "rtMax", "mzMin", "mz", "mzMax") %in% colnames(ROI))) {
+        saveEICsROI   <- TRUE
+      } else { if(verbose) {message('ROI columns must contain "cpdID", "cpdName", "rtMin", "rt", "rtMax", "mzMin", "mz" and "mzMax", EICs of ROI windows will not be saved')} }
+    } else { if(verbose) {message('ROI is not a data.frame, EICs of ROI windows will not be saved')} }
+  } else { if(verbose) {message('No ROI provided, EICs of ROI windows will not be saved')} }
+  
+  # calculate mean IS RT if IS_ROI is properly defined
+  calculateMeanISRT  <- FALSE
+  if(!is.null(IS_ROI)) {
+    # ROI is a data.frame
+    if(is.data.frame(IS_ROI)) {
+      # ROI data.frame columns
+      if(all(c("cpdID", "cpdName", "rtMin", "rt", "rtMax", "mzMin", "mz", "mzMax") %in% colnames(IS_ROI))) {
+        calculateMeanISRT  <- TRUE
+      } else { if(verbose) {message('IS_ROI columns must contain "cpdID", "cpdName", "rtMin", "rt", "rtMax", "mzMin", "mz" and "mzMax", mean RT of IS will not be calculated')} }
+    } else { if(verbose) {message('IS_ROI is not a data.frame, mean RT of IS will not be calculated')} }
+  } else { if(verbose) {message('No IS_ROI provided, mean RT of IS will not be calculated')} }
+  
+  # summary
+  if(verbose){
+    # save ROI EICs
+    if(saveEICsROI) {
+      message(paste("- EICs for each ROI windows will be saved to:", saveFolder))
+      message(paste("    ",dim(ROI)[1]," ROI in ", length(referenceSpectraFiles), " reference samples", sep=''))
+    } else {
+      message("- EICs of ROI windows will not be saved")
+    }
+    # calculate IS mean RT
+    if(calculateMeanISRT) {
+      message(paste("- Mean RT of IS across reference samples will be saved to:", saveFolder))
+      message(paste("    ",dim(IS_ROI)[1]," IS in ", length(referenceSpectraFiles), " reference samples", sep=''))
+    } else {
+      message("- Mean RT of IS will not be calculated")
+    }
+  }
+  
+  
+  ## save EICs ROI
+  if(saveEICsROI) {
+    if(verbose) {message('\n-- Saving EICs for each ROI --')}
+    nbROI <- dim(ROI)[1]
+    
+    # extract all ROIs in all files
+    all_ROIs <- lapply(referenceSpectraFiles, function(x) {
+      # if file doesn't exist, pass
+      singleSpectraDataPath   <- normalizePath(x, mustWork=FALSE)
+      if (!file.exists(singleSpectraDataPath)) {
+        message('File \"', singleSpectraDataPath ,'\" does not exist')
+        return(NULL)
+      }
+      raw_data      <- MSnbase::readMSData(singleSpectraDataPath, centroided=TRUE, mode='onDisk')
+      ROIsDataPoint <- extractSignalRawData(raw_data, rt=ROI[,c('rtMin','rtMax')], mz=ROI[,c('mzMin','mzMax')], verbose=verbose)
+      return(ROIsDataPoint)
+    })
+    # remove failures
+    specToKeep    <- sapply(all_ROIs, function(x) {!is.null(x)})
+    all_ROIs      <- all_ROIs[specToKeep]
+    sampleColour  <- sampleColour[specToKeep]
+    
+    # generate and save plots
+    for(i in seq(nbROI)) {
+      tmp_ROI_datapoints  <- unlist(lapply(all_ROIs, function(x,y) {x[y]}, y=i), recursive=FALSE)
+      tmp_EIC_plot        <- plotEICFit(ROIDataPointSampleList = tmp_ROI_datapoints, curveFitSampleList=NULL, rtMin=NULL, rtMax=NULL, sampling=250, sampleColour=sampleColour, verbose=FALSE)
+      # add box limits and apex
+      tmp_EIC_plot <- tmp_EIC_plot + ggplot2::geom_vline(ggplot2::aes(xintercept=c(ROI[i,'rtMin'], ROI[i,'rtMax'])), colour='darkgrey', linetype='dashed')
+      tmp_EIC_plot <- tmp_EIC_plot + ggplot2::geom_vline(ggplot2::aes(xintercept=ROI[i,'rt']), colour='red')
+      tmp_EIC_plot <- tmp_EIC_plot + ggplot2::ggtitle(paste(ROI[i,'cpdID'], '-', ROI[i,'cpdName'], '|', ROI[i,'rtMin'], '-', ROI[i,'rtMax'], 's |', ROI[i,'mzMin'], '-', ROI[i,'mzMax'], 'm/z'))
+      
+      # save
+      saveFileName  <- paste(ROI[i,'cpdID'],'.png',sep='')
+      ggplot2::ggsave(file=saveFileName, plot=tmp_EIC_plot, device='png', path=saveFolder, dpi=100, width=25, height=25, units='cm', limitsize=FALSE)
+    }
+    if(verbose) { message(paste(nbROI, 'ROIs saved to', saveFolder)) }
+  }
+  
+  
+  ## calculate mean IS RT
+  if(calculateMeanISRT) {
+    if(verbose) {message('\n-- Calculating mean RT for each IS --')}
+    IS_annotation         <- peakPantheRAnnotation(spectraPaths=referenceSpectraFiles, targetFeatTable=IS_ROI)
+    IS_annotation_results <- peakPantheR_parallelAnnotation(IS_annotation, ncores=ncores, verbose=verbose)
+    IS_annotation         <- IS_annotation_results$annotation
+    
+    # save IS fit diagnostic plots
+    if(saveISPlots) {
+      outputAnnotationDiagnostic(IS_annotation, saveFolder=file.path(saveFolder, 'IS_search/'), savePlots=TRUE, sampleColour=sampleColour, verbose=verbose, ncores=ncores)
+    }
+    
+    # calculate statistics
+    mean_IS_rt            <- data.frame(colMeans(annotationTable(IS_annotation, column='rt'), na.rm=TRUE))
+    colnames(mean_IS_rt)  <- 'mean_rt'
+    # save to disk
+    path_meanRT           <- file.path(saveFolder, '/IS_mean_RT.csv', sep='')
+    utils::write.csv(mean_IS_rt, file=path_meanRT, row.names=TRUE, fileEncoding="UTF-8")
+    if (verbose) { message('IS mean RT saved at ', path_meanRT) }
+  }
+}
--- dev_peakPantheR.R
+++ dev_peakPantheR.R
@@ -1,8 +1,8 @@
-## -------------------------------------------------------------------------------------------
-##    dev peakPantheR - detect and integrate pre-defined features in MS files using XCMS3
-## -------------------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
+##    dev peakPantheR - detect and integrate pre-defined features in MS files
+## -------------------------------------------------------------------------------
 
-library(devtools)  #install.packages('devtools')
+library(devtools)  #install.packages('devtools') / devtools::install_github("r-lib/devtools")
 library(roxygen2)  #install.packages('roxygen2')
 
 #----------------
@@ -17,12 +17,21 @@ package_dir = getwd()
 setwd(package_dir)
 
 #setRepositories(ind=1:2)
+#devtools::use_testthat()
 devtools::document()
 devtools::test()
-devtools::check()
+devtools::check()           # destroy inst/doc
+devtools::build_vignettes() # recreate inst/doc
 devtools::build()
 devtools::install()
 
+#devtools::use_vignette("getting-started")
+#devtools::use_vignette("real-time-annotation")
+#devtools::use_vignette("parallel-annotation")
+#devtools::build_vignettes()
+
+
+library(peakPantheR)
 
 remove.packages('peakPantheR')
 #------------------------------------------------
@@ -34,3 +43,4 @@ library(peakPantheR)
 setwd('./R')
 lapply(list.files(pattern = "[.][rR]$", recursive = TRUE), source)
 setwd('..')
+# ------------------------------------------------
