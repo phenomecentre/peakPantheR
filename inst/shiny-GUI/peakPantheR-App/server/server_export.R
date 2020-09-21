0a4e098c8eec608adef28c2dcf27531e48b5bca0
Arnaud M. Wolfer <adwolfer@gmail.com>
2020-09-21 22:43:44 +0200

    export UI, only appears if import took place

--- inst/shiny-GUI/peakPantheR-App/server/server_export.R
+++ inst/shiny-GUI/peakPantheR-App/server/server_export.R
@@ -6,6 +6,38 @@
 # - export results (tables to disk)
 # - export plot to disk
 
-# TODO: export server
 
-#exportUI
+## not annotated
+output$noImportForExportUI <- renderUI ({
+  if(importSuccess()=='yes') return()
+  tagList(
+    HTML("<div class=\"alert alert-dismissible alert-danger\"> <button type=\"button\" class=\"close\" data-dismiss=\"alert\">×</button> <h4 style=\"font-weight:bold\">No annotation imported</h4>Create or load a <i>peakPantheRAnnotation</i></div>")#,
+    #includeHTML("data/aboutExport.html")
+  )
+})
+
+
+## show the status of the peakPantheRAnnotation (need to be separated from the previous panel so they can be hidden)
+output$showAnnotStatusExp <- renderUI({
+  # Capture the annotation shown and split by line into a list
+  tmp_text  <- annotation_showText_UI_helper(annotation_showMethod_UI_helper(values$annotation))
+  fail_text <- paste(dim(values$failures)[1], 'annotation failure(s)')
+  # render the panel
+  wellPanel(
+    h4('Status:', style="color:#3e648d;font-weight:bold"),
+    helpText(tmp_text[[1]],style="color:black"),
+    helpText(tmp_text[[2]],style="color:black"),
+    helpText(tmp_text[[3]],style="color:black"),
+    helpText(tmp_text[[4]],style="color:black"),
+    helpText(tmp_text[[5]],style="color:black"),
+    helpText(tmp_text[[6]],style="color:black"),
+    tags$hr(),
+    if (!is.null(values$failures)) {
+      helpText(fail_text,style="color:black")
+    }
+  )
+})
+
+
+
+
--- inst/shiny-GUI/peakPantheR-App/ui/ui_export.R
+++ inst/shiny-GUI/peakPantheR-App/ui/ui_export.R
@@ -2,10 +2,64 @@
 
 tabPanel("Export results",
 
-  h3('Export', style="color:#3e648d;font-weight:bold")
-  #uiOutput("exportUI")
+# Conditional UI based on import successfully initialised
+  uiOutput("noImportForExportUI"),
+  conditionalPanel(
+    condition = "output.importDone=='yes'",
+    fluidRow(
+
+  ## Sidebar
+      column(width=2,
+        # Current annotation status
+        uiOutput("showAnnotStatusExp")
+      ), # end Sidebar column
+
+  ## Main panel
+      column(10,
+        h3('Export Data', style="color:#3e648d;font-weight:bold"),
+
+        # Row export inputs
+        fluidRow(
+          column(4,
+            # TODO: export .RData
+          ), # end column
+          column(4,
+            # TODO: export text .csv(s)
+          ), # end column
+          column(4,
+            # TODO: export .csv(s) boxes
+          ) # end column
+        ),  # end fluidRow Export inputs
+        tags$hr(),
+        br(),
+
+        h3("Export Annotation Results and Diagnostic Plots",style="color:#3e648d;font-weight:bold"),
+        helpText("Automated summarisation step, only possible if Shiny is running on a local machine.", style="color:#666666"), # See ",em("Code")," to run from the command line.
+        # TODO: fix the text for annotation result output
+
+        # Row export results
+        fluidRow(
+          column(6,
+            h4("Save Diagnostic Plots:"),
+            helpText("Stores all P-values calculated in multiple ",em(".csv")," file. ( default",em("summary_pvalue-all.csv"),")", style="color:#666666")
+          # TODO: change Export Diagnostic plots text
+          ), # end column
+          column(6,
+            h4("Save Annotation Values:"),
+            helpText("Plots and save to disk all variables with a P-value inferior to a given cut-off.", style="color:#666666")
+          # TODO: change Export Annotation Values text
+          ) # end column
+        ),  # end fluidRow Export results Text
+
+        # The result export panel only exist if annotation took place
+        column(12,
+          uiOutput("exportPvalFigUI"),
+          # TODO: a result export panel only appearing if is annotated!
+        tags$hr()
+        )   # end column export results
+
+      )     # end Main panel column
+    )       # end fluidRow (sidebar + menu)
+  )         # end conditional panel
 )
-
-# TODO: export UI
-
 # end EXPORT Tab panel  ------------------------------------------------------ #
